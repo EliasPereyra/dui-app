03079204fa87d702069fe952e0101ee5af71ad85
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-03-04 22:03:43 -0300

    Style: change the bg color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,28 @@
+import Image from "next/image";
+import { circle, stack } from "../../styled-system/patterns";
+
 export function Navbar() {
   return (
-    <nav>
-      <img src="" alt="logo" />
-      <div id="tools"></div>
+    <nav
+      className={stack({
+        direction: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        bg: "linear-gradient(to right, #131417 0%, #06080C 100%)",
+      })}
+    >
+      <div className={circle({ overflow: "hidden" })}>
+        <Image
+          src={"https://via.placeholder.com/150"}
+          alt="Logo"
+          width="50"
+          height="50"
+        />
+      </div>
+      <div id="tools">
+        <div>1</div>
+      </div>
       <div id="users"></div>
     </nav>
-  )
-} 
+  );
+}
