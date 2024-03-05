021402e3b6c9130dc45be8a6f5065cc1e937925f
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-03-04 22:07:00 -0300

    Add extra configs for webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      "uf8-8-validate": "commmonjs utf-8-validate",
+      butfferutil: "commonjs bufferutil",
+      canvas: "commonjs canvas",
+    });
+
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+        port: "",
+        pathname: "/150",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
