2e22c806f8701490e4aeaa04b4af9553bebc8c10
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-03-21 23:33:30 -0300

    Add styles to the playground component

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -1,10 +1,17 @@
+import { css } from "../../styled-system/css";
+
 export function Playground({
   canvasRef,
 }: {
   canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
 }) {
   return (
-    <section>
+    <section
+      className={css({
+        bg: "#333",
+        w: "100%",
+      })}
+    >
       <canvas ref={canvasRef} />
     </section>
   );
