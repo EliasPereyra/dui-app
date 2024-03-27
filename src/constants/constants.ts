a3afba8b3e4b70892fa6e944ce89b2cce6dd95e9
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-03-27 16:26:07 -0300

    Create a file for all the constant elements

---
+++ src/constants/constants.ts
@@ -0,0 +1,57 @@
+export const fontFamilyOptions = [
+  {
+    name: "Inter",
+    value: "Inter",
+  },
+  {
+    name: "Helvetica",
+    value: "Helvetica",
+  },
+  {
+    name: "Poppins",
+    value: "Poppins",
+  },
+];
+
+export const fontSizeOptions = [
+  {
+    value: 10,
+    label: 10,
+  },
+  {
+    value: 12,
+    label: 12,
+  },
+  {
+    value: 14,
+    label: 14,
+  },
+  {
+    value: 16,
+    label: 16,
+  },
+  {
+    value: 18,
+    label: 18,
+  },
+  {
+    value: 20,
+    label: 20,
+  },
+  {
+    value: 24,
+    label: 24,
+  },
+  {
+    value: 28,
+    label: 28,
+  },
+  {
+    value: 32,
+    label: 32,
+  },
+  {
+    value: 36,
+    label: 36,
+  },
+];
