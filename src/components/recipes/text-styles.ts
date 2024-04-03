bf1e53ef9c972b57dfbf78adb9fa5875bfc39782
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-04-02 23:23:39 -0300

    Create a recipe for custom text styles

---
+++ src/components/recipes/text-styles.ts
@@ -0,0 +1,15 @@
+import { defineTextStyles } from "@pandacss/dev";
+
+export const textStyles = defineTextStyles({
+  body: {
+    value: {
+      fontSize: ".9rem",
+      fontWeight: "300",
+    },
+  },
+  h3: {
+    value: {
+      fontSize: "1.1rem",
+    },
+  },
+});
